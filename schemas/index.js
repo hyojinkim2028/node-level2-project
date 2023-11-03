5251634c96364c5e32b4fa6727009beaa964ff2f
rizzy <12hyojinkim14@gmail.com>
2023-11-03 17:46:46 +0900

    fix db

--- schemas/index.js
+++ schemas/index.js
@@ -2,19 +2,12 @@ const mongoose = require('mongoose');
 
 const connect = () => {
   mongoose
-    .connect('mongodb://root:1214@localhost:27017/admin?authSource=admin', {
-      dbName: 'productProject',
-      useNewUrlParser: true,
-    })
-    .then(() => {
-      console.log('몽고디비 연결 성공');
-    })
-    .catch((err) => {
-      console.log('몽고디비 연결 에러', err);
-    });
+    .connect('mongodb://localhost:27017/spa_mall')
+    .catch((err) => console.log(err));
 };
+
 mongoose.connection.on('error', (err) => {
-  console.error('MongoDB 연결 에러', err);
+  console.error('몽고디비 연결 에러', err);
 });
 
 module.exports = connect;
