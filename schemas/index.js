63e528df96bea3f6624f0ab178861e6e7550249b
rizzy <12hyojinkim14@gmail.com>
2023-11-03 17:27:00 +0900

    db 수정

--- schemas/index.js
+++ schemas/index.js
@@ -2,16 +2,17 @@ const mongoose = require('mongoose');
 
 const connect = () => {
   mongoose
-    .connect(
-      'mongodb://root:1214@localhost:27017/?authMechanism=DEFAULT&authSource=admin',
-      {
-        dbName: 'node_lv1',
-      }
-    )
-    .then(() => console.log('MongoDB 연결에 성공하였습니다.'))
-    .catch((err) => console.log(`MongoDB 연결에 실패하였습니다. ${err}`));
+    .connect('mongodb://root:1214@localhost:27017/admin', {
+      dbName: 'productProject',
+      useNewUrlParser: true,
+    })
+    .then(() => {
+      console.log('몽고디비 연결 성공');
+    })
+    .catch((err) => {
+      console.log('몽고디비 연결 에러', err);
+    });
 };
-
 mongoose.connection.on('error', (err) => {
   console.error('MongoDB 연결 에러', err);
 });
