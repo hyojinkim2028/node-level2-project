c41593e3d9d8b8c27d1db0999439a1d73f4d4f11
rizzy <12hyojinkim14@gmail.com>
2023-11-05 21:12:02 +0900

    fix

--- schemas/index.js
+++ schemas/index.js
@@ -1,12 +1,13 @@
 const mongoose = require('mongoose');
+const dotenv = require('dotenv');
 // db 아이디, 비밀번호 정보 보안
 require('dotenv').config();
-const connectUrl = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.rywpqas.mongodb.net/?retryWrites=true&w=majority`
+const connectUrl = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.rywpqas.mongodb.net/?retryWrites=true&w=majority`;
 
 const connect = () => {
   mongoose
     .connect(connectUrl, {
-      dbName:'spa_mall',
+      dbName: 'spa_mall',
       useNewUrlParser: true,
       useUnifiedTopology: true,
     })
