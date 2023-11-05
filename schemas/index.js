91f88dfb38dbf63991123259c96dd20d07a63313
rizzy <12hyojinkim14@gmail.com>
2023-11-05 22:31:22 +0900

    mongodb 연결 코드 정리

--- schemas/index.js
+++ schemas/index.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const dotenv = require('dotenv');
+// const dotenv = require('dotenv');
 // db 아이디, 비밀번호 정보 보안
 require('dotenv').config();
 const connectUrl = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.rywpqas.mongodb.net/?retryWrites=true&w=majority`;
@@ -8,8 +8,8 @@ const connect = () => {
   mongoose
     .connect(connectUrl, {
       dbName: 'spa_mall',
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
+      // useNewUrlParser: true,
+      // useUnifiedTopology: true,
     })
     .then(() => {
       console.log('몽고디비 연결 성공');
