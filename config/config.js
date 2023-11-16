4a247fe6d839b16e3a7eae3427190add3ddeb5e8
rizzy <12hyojinkim14@gmail.com>
2023-11-16 20:28:23 +0900

    [Docs] 주석정리

--- config/config.js
+++ config/config.js
@@ -9,20 +9,20 @@ const development = {
   dialect: 'mysql',
 };
 
-// const test = {
-//   username: process.env.SQL_USERNAME,
-//   password: process.env.SQL_PASSWORD,
-//   database: 'database_test',
-//   host: process.env.SQL_HOST,
-//   dialect: 'mysql',
-// };
+const test = {
+  username: process.env.SQL_USERNAME,
+  password: process.env.SQL_PASSWORD,
+  database: 'database_test',
+  host: process.env.SQL_HOST,
+  dialect: 'mysql',
+};
 
-// const production = {
-//   username: process.env.SQL_USERNAME,
-//   password: process.env.SQL_PASSWORD,
-//   database: 'database_production',
-//   host: process.env.SQL_HOST,
-//   dialect: 'mysql',
-// };
+const production = {
+  username: process.env.SQL_USERNAME,
+  password: process.env.SQL_PASSWORD,
+  database: 'database_production',
+  host: process.env.SQL_HOST,
+  dialect: 'mysql',
+};
 
 module.exports = { development };
